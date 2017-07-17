73d37be2091a2e0922f30862b5b0f02a60504b4a
sahbia <ouaja.sahbia@gmail.com>
2017-07-17 12:01:09 +0200

    add affich function

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@ var t1=["img/m1.jpeg","img/m2.jpeg","img/m3.jpeg","img/m4.jpeg","img/m5.jpeg","i
 var t2=["img/m1.jpeg","img/m2.jpeg","img/m3.jpeg","img/m4.jpeg","img/m5.jpeg","img/m6.jpeg","img/m7.jpeg"];
 var tb1=["#14248a","#a53860","#fcab64","#4c191b","#e83151","#f0c808","#9EFD38"];
 var rang1=document.getElementsByClassName("imrang1");
-var newdiv=document.getElementsByClassName("newdiv");
+var div=document.getElementsByClassName("imaged1");
+// console.log(div1);
 var rang2=document.getElementsByClassName("imrang2");
-var newdiv2=document.getElementsByClassName("newdiv2");
+var div2=document.getElementsByClassName("imaged2");
 var score=0;
-for(i=0;i<7;i++)
-         {
-           newdiv[i].style.backgroundColor=tb1[i];
-           newdiv2[i].style.backgroundColor=tb1[i];
 
-         }
 
 //Math.floor(Math.random() * 10);
 function echange(t, x,y)
@@ -37,114 +33,170 @@ function melange(t)
 //change the image of the first div
 document.getElementById("jouer").addEventListener("click",function(){
     melange(t1);
-   
-         for(i=0;i<rang1.length;i++)
-         {
-         	    rang1[i].src=t1[i];
+    melange(t2);
 
-             
-              rang1[i].style.zIndex = "-100";
-         	  newdiv[i].style.zIndex = "100";
+               
+
+                for(let i=0;i<7;i++)
+             {
+                  div[i].style.backgroundColor=tb1[i];
+                  div2[i].style.backgroundColor=tb1[i];
+
+              }
+   
+         for(let j=0;j<7;j++)
+         {
+         	    rang1[j].src=t1[j];
+
+                rang2[j].src=t2[j];
+         	     rang2[j].style.opacity = "0";
+                 rang1[j].style.opacity = "0";
+         	  
               
          }
+               document.getElementById("score").innerHTML=0;
 
-   });      
-      
-      //  //noeudRemplacé = noeudParent.replaceChild(nouvelEnfant, ancienEnfant);
-      //  //replacedNode = parentNode.replaceChild(newChild, oldChild);
+   });  
+
+var src1="";
+var src2="";
+var firstimage="";
+function affich(img)
+  {
+
+     if(src1=="")
+     { src1=img.src;
+     	firstimage=img;
+       img.style.opacity=1;
+      console.log("src1= "+src1);
+     }
+    else 
+     {
+     	  src2=img.src;
+     	  img.style.opacity=1;
+     	  console.log("scr2= "+src2);
+     	     if(src1===src2)
+            {
+     	           score++;
+     	          document.getElementById("score").innerHTML=score;
+     	           img.style.opacity=1;
+     	           firstimage.style.opacity=1;
+     	           scr1="";
+     	           src2="";
+
+              }
+           else{
+     	            setTimeout(function(){
+     	  	            firstimage.style.opacity=0;
+     	  	             img.style.opacity=0;
+
+     	            },2000); 
+
+     	            src1="";
+     	           src2="";
+
+               }
+    
+     }
+
+     
+
+
+     
+
+
+  } 
+
+
+
+
+
+
+
+
+
 
 
 //change the image of the second div 
-   document.getElementById("jouer").addEventListener("click", function(){
-   	melange(t2);
+// document.getElementById("jouer").addEventListener("click", function(){
    
-         for(i=0;i<rang2.length;i++)
-         {
-         	rang2[i].src=t2[i];
-         	rang2[i].style.zIndex = "-100";
-           	newdiv2[i].style.zIndex = "100";
+   
+//          for(i=0;i<rang2.length;i++)
+//          {
          	
          	
-         }
+         	
+//          }
+   
 
-});
-//ACTION ONCLICK IN IMAGES
-// function montre(j)
-// {
-// 	 rang1[j].style.zIndex = "100";
-//      newdiv[j].style.zIndex = "-100";
-// }
-//var img=document.getElementsByClassName("imaged1");
- var trouve=0;
+// });
  
- for( let i=0;i<7;i++)
-   {     
+ // for( let i=0;i<7;i++)
+ //   {     
  	 
  	 
- 	  newdiv[i].addEventListener("click", function(){
+ // 	  newdiv[i].addEventListener("click", function(){
  	      
-          rang1[i].style.zIndex = "100";
+ //          rang1[i].style.zIndex = "100";
        
   
-          newdiv[i].style.zIndex = "-100"; 
-          for(let j=0;j<7;j++)
-          {
-          	if(i!=j){ newdiv[j].style.cursor="none";}
-          }
+ //          newdiv[i].style.zIndex = "-100"; 
+ //          for(let j=0;j<7;j++)
+ //          {
+ //          	if(i!=j){ newdiv[j].style.cursor="none";}
+ //          }
          
          
-                         for( let k=0;k<7;k++)
-                         {
-                                newdiv2[k].addEventListener("click", function(){
+ //                         for( let k=0;k<7;k++)
+ //                         {
+ //                                newdiv2[k].addEventListener("click", function(){
  	      
-                                    rang2[k].style.zIndex = "100";
+ //                                               rang2[k].style.zIndex = "100";
          
-					                           newdiv2[k].style.zIndex = "-100"; 
+	// 				                           newdiv2[k].style.zIndex = "-100"; 
 					                     
-					                           for(let j=0;j<7;j++)
-					                                   {
-					          	                          if(k!=j){ newdiv2[j].style.cursor="none";}
-					                                     } 
+	// 				                           for(let j=0;j<7;j++)
+	// 				                                   {
+	// 				          	                          if(k!=j){ newdiv2[j].style.cursor="none";}
+	// 				                                     } 
 
-					               console.log(rang2[k].src);
-					               console.log(rang1[i].src);
-					                       if(rang1[i].src=== rang2[k].src)
-					                       	   { score++;
-					                       	   	  document.getElementById("score").innerHTML=score;
+	// 				               //console.log(rang2[k].src);
+	// 				               //console.log(rang1[i].src);
+	// 				                       if(rang1[i].src===rang2[k].src)
+	// 				                       	   { score++;
+	// 				                       	   	  document.getElementById("score").innerHTML=score;
 
-					                             }  //else{ setTimeout() }
+	// 				                             }  else{ 
+	// 				                             	          setTimeout(function(){
+	// 				                             	          	        rang1[i].style.zIndex = "-100";
+ //                                                                       rang2[k].style.zIndex = "-100";
+ //                                                                                          },2000) ;}           
+            
+                      
+ //                                     for(let l=0;l<7;l++)
+ //                                       {
+ //                                       	  newdiv[l].style.cursor="default";
+ //          	                             newdiv2[l].style.cursor="default";
+          	                             
+ //          	                            } 
+             
 					                  
-					              });
+	// 				              });
                                 
            
-                          }
+ //                          }
 
-          });
+ //          });
          
-      //    setTimeout(function(){ { rang1[i].style.zIndex = "-100";
-      //                                                        rang2[k].style.zIndex = "-100";}
-                                                                                                     
-      //       else{trouve++; console.log(trouve) ; rang1[i].style.zIndex = "1000";
-      //                                                        rang2[k].style.zIndex = "1000";}
+      
+ // }  
+   
 
-                      
-      //     for(let l=0;l<7;l++)
-      //      {
-      //     	newdiv2[l].style.cursor="default"; newdiv[l].style.cursor="default";}  },2000);
-              
-           
+
+ 
+   //test code
    
- }  
-   
-     //  console.log(rang1[i].alt);
-     //  console.log(rang2[i].alt);
-     // if(rang1[i].alt===rang2[i].alt){ scor++;
-     //  console.log(scor);} 
-     // else{
-     	
-     //     newdiv[i].style.zIndex = "100";
-        
-     //   newdiv2[i].style.zIndex = "100";
-     // }      
+
+
 
 
